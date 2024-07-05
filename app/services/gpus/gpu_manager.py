93defd83c9f8e4b8b19e3ddc82bea6aca27cbd33
jayslachance <ja_sonlachance@hotmail.com>
2024-07-05 14:49:19 -0400

    refactor method name, and method to get gpu with multiple processes

--- app/services/gpus/gpu_manager.py
+++ app/services/gpus/gpu_manager.py
@@ -11,7 +11,7 @@ class GPUManager:
             return int(output.strip())
         return 0
 
-    def check_gpu_process(self, host, username, password):
+    def get_idle_gpus(self, host, username, password):
         command = "nvidia-smi --query-compute-apps=gpu_uuid --format=csv,noheader,nounits"  # Check for any processes running
         try:
             output = execute_ssh_command(host, username, password, command)
@@ -32,4 +32,50 @@ class GPUManager:
             return idle_gpus
         except Exception as e:
             print(f"An error occurred: {e}")
+            return []
+
+    def get_running_gpus(self, host, username, password):
+        try:
+            # Command to get the UUIDs of GPUs currently running processes
+            command = "nvidia-smi --query-compute-apps=gpu_uuid --format=csv,noheader,nounits"
+            
+            # Execute the command
+            output = execute_ssh_command(host, username, password, command)
+            
+            # Process the output
+            if output.strip():  # If there is output, process it
+                running_gpu_uuids = set(output.strip().split('\n'))
+            else:  # If output is empty, no GPUs are running processes
+                running_gpu_uuids = set()
+            
+            return list(running_gpu_uuids)
+        except Exception as e:
+            print(f"An error occurred while retrieving running GPUs: {e}")
+            return []
+
+    def get_gpus_with_multiple_processes(self, host, username, password):
+        try:
+            # Command to get detailed information about processes using GPUs
+            command = "nvidia-smi --query-compute-apps=gpu_uuid --format=csv,noheader,nounits"
+            output = execute_ssh_command(host, username, password, command)
+            
+            if not output.strip():
+                return []  # No processes running
+            
+            # Split the output into lines
+            gpu_uuids = output.strip().split('\n')
+            # Count the number of processes per GPU
+            gpu_process_count = {}
+            for gpu_uuid in gpu_uuids:
+                if gpu_uuid in gpu_process_count:
+                    gpu_process_count[gpu_uuid] += 1
+                else:
+                    gpu_process_count[gpu_uuid] = 1
+            
+            # Filter GPUs with 2 or more processes
+            gpus_with_multiple_processes = [gpu_uuid for gpu_uuid, count in gpu_process_count.items() if count >= 2]
+            
+            return gpus_with_multiple_processes
+        except Exception as e:
+            print(f"An error occurred while retrieving GPUs with multiple processes: {e}")
             return []
